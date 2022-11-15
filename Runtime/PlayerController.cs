94e9693ed4b81c529d7e1287ef934afa739004ba
unknown <c00248444@itcarlow.ie>
2022-11-15 10:40:26 +0000

    P1-2 Attack

--- Runtime/PlayerController.cs
+++ Runtime/PlayerController.cs
@@ -17,15 +17,29 @@ public class PlayerController : MonoBehaviour
         instance = this;
     }
 
+    public static void EstablishWeapon(GameObject weapon)
+    {
+        weapon.SetActive(false);
+    }
+
+    public static void Attack(GameObject weapon)
+    {
+        if (!instance.inCouroutine)
+        {
+            instance.inCouroutine = true;
+            instance.StartCoroutine(instance.AttackCoroutine(weapon));
+        }
+    }
+
     public static void Duck(Rigidbody2D rb)
     {
-        if(!instance.inCouroutine)
+        if (!instance.inCouroutine)
         {
             instance.ducking = true;
             instance.inCouroutine = true;
             instance.StartCoroutine(instance.DuckCoroutine(rb));
         }
-        
+
     }
 
     public static void CancelDuck()
@@ -60,6 +74,24 @@ public class PlayerController : MonoBehaviour
             rb.transform.localScale = scale;
         }
         rb.transform.localScale = startScale;
+        instance.inCouroutine = false;
+    }
+
+    IEnumerator AttackCoroutine(GameObject weapon)
+    {
+        weapon.SetActive(true);
+        Quaternion rotation = weapon.transform.rotation;
+        float startRotation = rotation.z;
+        rotation *= Quaternion.Euler(0, 0, 75);
+        while (rotation.z > startRotation)
+        {
+            rotation *= Quaternion.Euler(0, 0, -25);
+            weapon.transform.rotation = rotation;
+            yield return new WaitForSeconds(0.1f);
+        }
+        rotation.z = startRotation;
+        weapon.transform.rotation = rotation;
+        weapon.SetActive(false);
         instance.inCouroutine = false;
     }
 }
--- Tests/ControllerTests.cs
+++ Tests/ControllerTests.cs
@@ -41,4 +41,25 @@ public class ControllerTests
         yield return new WaitForSeconds(0.2f);
         Assert.AreEqual(controlY, rb.transform.localScale.y);
     }
+
+    [UnityTest]
+    public IEnumerator WeaponSets()
+    {
+        GameObject weapon = new GameObject();
+        PlayerController.EstablishWeapon(weapon);
+        Assert.IsFalse(weapon.gameObject.activeSelf);
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator Attack()
+    {
+        GameObject weapon = new GameObject();
+        Quaternion rotation = weapon.transform.rotation;
+        PlayerController.Attack(weapon);
+        yield return new WaitForSeconds(0.2f);
+        Quaternion newRotation = weapon.transform.rotation;
+        Assert.IsTrue(weapon.gameObject.activeSelf);
+        Assert.Greater(newRotation.z, rotation.z);
+    }
 }
